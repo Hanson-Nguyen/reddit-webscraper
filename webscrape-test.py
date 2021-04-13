4f711abecf6c3a098830e6b04f072ceb9bc38179
Hanson Nguyen <hansonn00@gmail.com>
2021-04-13 19:54:09 -0400

    Update webscrape-test.py

--- webscrape-test.py
+++ webscrape-test.py
@@ -7,8 +7,23 @@ page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, 'html.parser')
 
-shop_elems = soup.find_all('div', class_ = 'col-md-9')
+shop_elems = soup.find_all('div', class_ = 'col-sm-4 col-lg-4 col-md-4')
 
-for shop_elems in shop_elems:
-    items_elem = shop_elems.find('div', class_ = 'row')
-    print(items_elem.text)
+for item_elems in shop_elems:
+
+    items_name = item_elems.a.text
+    items_cost = item_elems.h4.text
+    items_desc = item_elems.p.text
+
+    rating_div = item_elems.find('div', {'class': 'ratings'})
+    rating_tag = rating_div.find('p')
+
+    items_reviews = rating_tag.text
+
+    print(items_name)
+    print(items_cost)
+    print(items_desc)
+    print(items_reviews)
+    print()
+
+#print(items_stars + " stars")
